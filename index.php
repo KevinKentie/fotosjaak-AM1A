fdce483a2ade61b115bf0be0d37f36be4b691082
Kevin <kevin-kentie@hotmail.com>
2013-11-19 14:41:15 +0100

    derede regel erbij en eerste regel aangepast

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?
-	echo "Dit is me eerste regel";
-	echo "dit is mijn 2e regel"
+	echo "Dit is me eerste regel maar deze heb ik gewijzigd";
+	echo "dit is mijn 2e regel";
+	echo "dit is mijn 3e regel";
 ?>
